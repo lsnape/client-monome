5437b76ea563ea0c2c53fdde1b4594b4777a99de
Luke Snape <ldjsnape@gmail.com>
2015-06-14 19:22:27 +0100

    Initial commit

---
+++ src/client_monome/core.clj
@@ -0,0 +1,6 @@
+(ns client-monome.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/client_monome/core_test.clj
@@ -0,0 +1,7 @@
+(ns client-monome.core-test
+  (:require [clojure.test :refer :all]
+            [client-monome.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
