e4f156e4b2b178abd39be8e15b83caa707cb8534
Luke Snape <ldjsnape@gmail.com>
2015-06-14 19:23:32 +0100

    Pulling in aleph

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[aleph "0.4.0"]
+                 [org.clojure/clojure "1.6.0"]])
